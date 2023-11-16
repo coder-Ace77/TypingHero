3103fa69bdd29e2b3292280651c7435e8c181a7f
SaMMYFrosT <sammy.frost.221b@gmail.com>
2023-11-16 15:49:32 +0530

    added logo and time

---
+++ src/header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import logo from "./images/keyboard.png";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="grid-container">
+        <div className="grid-item header-logo">
+          <img className="logo" src={logo} alt="Not working" />
+        </div>
+        <div className="grid-item"></div>
+        <div className="grid-item test-duration">
+          <span>Test Duration: </span>
+          <div className="test-times">
+            <span>
+              <button className="time-duration-button" id="selected">
+                0:30
+              </button>
+            </span>
+            <span>
+              <button className="time-duration-button">1:00</button>
+            </span>
+            <span>
+              <button className="time-duration-button">1:30</button>
+            </span>
+            <span>
+              <button className="time-duration-button">3:00</button>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/logo.js
@@ -0,0 +1,11 @@
+// import React from "react";
+// import logo from "./images/keyboard.png";
+// import "./logo.css";
+
+// const Logo = () => {
+//   return (
+
+//   );
+// };
+
+// export default Logo;
