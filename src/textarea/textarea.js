046e7a6bb6fe4fc833010cfe694f4d2de7553031
coder-Ace77 <adil7k777@gmail.com>
2023-12-12 18:57:44 +0530

    Removed sidebar

--- src/textarea/textarea.js
+++ src/textarea/textarea.js
@@ -1,38 +1,12 @@
 import React, { useState, useRef } from "react";
-import { NavLink } from "react-router-dom";
 
 import './textarea.css';
-import TextareaAutosize from 'react-textarea-autosize';
 
 import { FaBarsProgress } from "react-icons/fa6";
 import { IoSettings, IoColorFill } from "react-icons/io5";
 import { FaAddressBook } from "react-icons/fa";
 
 var text = "lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good mylorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my";
-
-const routes = [
-    {
-        path:"/about",
-        name:'About',
-        icon : <FaBarsProgress/>
-    },
-    {
-        path:"/graph",
-        name:'Graph',
-        icon : <FaBarsProgress/>
-    },
-    {
-        path:"/settings",
-        name:'About',
-        icon : <FaBarsProgress />
-    },
-    {
-        path:"/theme",
-        name:'Theme',
-        icon : <FaBarsProgress />
-    }
-]
-
 
 function TextArea() {
 
@@ -51,16 +25,6 @@ function TextArea() {
 
     return (
         <div className="main-box">
-            <div className="side-menu">
-                <div className="settings-icon" >
-                <IoSettings size={50} />
-                </div>
-            <FaBarsProgress size={50}  />
-            <IoSettings size={50} />    
-            <FaAddressBook size={50}/>
-            <IoColorFill size={50}/>
-
-                </div>
             <div className="text-box" contentEditable="true" spellCheck="false" onKeyDownCapture={handleInputChange} ref={main_text}>
 
                 {inputText}
