14d16b6072f03975063c0f121b939576433e882e
coder-Ace77 <adil7k777@gmail.com>
2023-12-12 21:43:57 +0530

    Textarea in porgress

--- src/textarea/textarea.js
+++ src/textarea/textarea.js
@@ -1,35 +1,84 @@
 import React, { useState, useRef } from "react";
-
 import './textarea.css';
 
-import { FaBarsProgress } from "react-icons/fa6";
-import { IoSettings, IoColorFill } from "react-icons/io5";
-import { FaAddressBook } from "react-icons/fa";
-
-var text = "lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good mylorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my";
+var text = "$lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good mylorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my";
+var ctext = text;
+var textarray = text.split("");
+var index = 0;
 
 function TextArea() {
 
-    const [inputText, setInputText] = useState(text);
-    const main_text = useRef(null);
-    let index = 0;
+    const [inputText, setInputText] = useState("\u007C" + text.substring(1));
+    const [inputFocus, setInputFocus] = useState(true);
+    const inputRef = useRef(null);
+
+    const backspaceHandler = (e) => {
+        if (e.key != "Backspace") {
+            return true;
+        }
+
+        if (index <= 0) {
+            index -= 1;
+            return true;
+        }
+        textarray[index] = ctext[index];
+        textarray[index - 1] = "\u007C";
+        index -= 1;
+        return false;
+    }
+
+    const validateKey = (e) => {
+        if (e.key.length > 1) {
+            return false;
+        }
+        return true;
+    }
+
+    const checkspace = (e) => {
+        if (e.key == " " || ctext[index + 1] == " ") {
+            if (e.key == ctext[index + 1])
+                return true;
+            else
+                return false;
+        }
+        return true;
+    }
+
 
     const handleInputChange = (e) => {
-        text = "111";
-        console.log("Fired!!!", e.key);
-        index += 1;
-        text = text.substring(index);
-        main_text.current.innerText = text;
-        setInputText("");
+
+        var x = true;
+        // backspace handler
+        x = backspaceHandler(e);
+
+        // handle and validate input
+        x = x && validateKey(e);
+
+        // check space and non space
+        x = x && checkspace(e);
+
+        // new line
+        if (x) {
+            textarray[index] = e.key;
+            textarray[index + 1] = "\u007C";
+            index += 1;
+        }
+        text = textarray.join("");
+        setInputText(text);
+    };
+
+    function focusHandler(e) {
+        console.log("Focus");
+        inputRef.current.focus();
     };
 
     return (
         <div className="main-box">
-            <div className="text-box" contentEditable="true" spellCheck="false" onKeyDownCapture={handleInputChange} ref={main_text}>
-
+            <div className="text-box" onClick={focusHandler}>
                 {inputText}
             </div>
-
+            <input onKeyDown={handleInputChange} autoFocus={true} className="inputBox" ref={inputRef}>
+            </input>
         </div>
     );
 }
