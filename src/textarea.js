df6dbbb401967f8dc3ad96a589bb7ab9471dce46
coder-Ace77 <adil7k777@gmail.com>
2023-12-12 17:57:16 +0530

    Textarea

--- src/textarea.js
+++ src/textarea.js
@@ -1,21 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import './textarea.css';
-import TextareaAutosize from 'react-textarea-autosize';
 
-const text = "lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good mylorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my";
+var text = "lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good mylorem ipsum hello world my name is good my lorem ipsum hello world my name is good my lorem ipsum hello world my name is good my";
 
 function TextArea() {
 
-    const [inputText, setInputText] = useState('');
+    const [inputText, setInputText] = useState(text);
+    const main_text = useRef(null);
+    let index = 0;
 
     const handleInputChange = (e) => {
-        setInputText(e.target.value);
+        text = "111";
+        console.log("Fired!!!", e.key);
+        index += 1;
+        text = text.substring(index);
+        main_text.current.innerText = text;
+        setInputText("");
     };
 
     return (
         <div className="main-box">
-            <div className="text-box" contentEditable="true" spellCheck="false">
-                {text}
+            <div className="text-box" contentEditable="true" spellCheck="false" onKeyDownCapture={handleInputChange} ref={main_text}>
+
+                {inputText}
             </div>
         </div>
     );
